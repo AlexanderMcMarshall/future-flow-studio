04ac6d65511876b14f4b62027fb14194f9d112d0
Alexandru Bujor <alexandru.bujor@isa.utm.md>
2026-03-16 15:47:25 +0200

    dev1

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/future-flow-studio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
